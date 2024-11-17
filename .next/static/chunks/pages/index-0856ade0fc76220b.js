0c829fb2d870088862495c78a777b64477b4cff2
Lucas Gabriel <lcs.gabriel39@gmail.com>
2024-11-17 20:35:41 -0300

    update node_modules

--- .next/static/chunks/pages/index-0856ade0fc76220b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[332],{2022:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(6887)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(2022)),_N_E=_.O()}]);
